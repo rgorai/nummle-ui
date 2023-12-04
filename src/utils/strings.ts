4775d4edffc6c41f583f59fbe12c3990242b6198
Ron Gorai <ron.gorai2020@gmail.com>
2023-12-04 16:48:49 -0500

    saving updates

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,5 @@
+import { APP_NAME } from './env'
+
 export const capitalizeFirstLetter = (string: string): string =>
   string.length > 0 ? string[0].toUpperCase() + string.slice(1) : ''
 
@@ -5,4 +7,11 @@ export const transformTomTomPhoneNumber = (phone: string): string => {
   const [_, number] = phone.split(' ')
   const [code, num1, num2] = number.split('-')
   return `(${code}) ${num1}-${num2}`
+}
+
+export const setDocumentTitle = (...args: string[]) => {
+  document.title = `${args.reduce(
+    (p, c) => `${p} ${p !== '' ? '|' : ''} ${c}`,
+    ''
+  )} | ${APP_NAME}`
 }
