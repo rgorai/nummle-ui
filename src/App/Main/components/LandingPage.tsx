96b936dd4deef5ab1fc76903aa409e3c29323eb2
Dominick <Dbattine@stevens.edu>
2024-01-25 18:05:12 -0500

    Update LandingPage.tsx
    Test

--- src/App/Main/components/LandingPage.tsx
+++ src/App/Main/components/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { APP_NAME } from '../../../utils/env'
 
 const LandingPage = () => {
-  return <div>{`Welcome to ${APP_NAME}`}</div>
+  return <div>{`Welcome to hi zack ${APP_NAME}`}</div>
 }
 
 export default LandingPage
