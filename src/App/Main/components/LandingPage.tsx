ba456aff7b353d3ab90eedb129e48fa3be259d81
ZackRimshnick <zrimshnick@gmail.com>
2024-01-25 19:28:50 -0500

    message

--- src/App/Main/components/LandingPage.tsx
+++ src/App/Main/components/LandingPage.tsx
@@ -1,7 +1,29 @@
 import { APP_NAME } from '../../../utils/env'
-
+import React, { useState } from 'react'
+import styles from '../styles/landingPage.module.scss'
 const LandingPage = () => {
-  return <div>{`Welcome to ${APP_NAME}`}</div>
+  const [userInput, setUserInput] = useState('')
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(event.target.value)
+  }
+
+  return (
+    <>
+      <div className={styles.intro}>
+        <div
+          className={styles.slogan}
+        >{`See what your friends and locals are saying right now about restaurants near you`}</div>
+        <input
+          type="text"
+          value={userInput}
+          onChange={handleInputChange}
+          placeholder="Enter delivery address"
+          className={styles.addressBox}
+        />
+      </div>
+    </>
+  )
 }
 
 export default LandingPage
